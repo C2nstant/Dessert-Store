5c22093152b8dce70d653a1fa4d7a68af4eb1a10
C2nstant <earljaissendizon@gmail.com>
2025-11-29 20:21:55 +0800

    Module 2 Commit
    + Conditionals (if else)
    + Loops (so that the lines will be less on repeating html)
    + Includes (for the footer)
    + Quantity and Availability Table Header
    + Special Offers
    + Extra design for the Special Offers (unresponsive)

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer>
+    <h3>&copy; 2025 Forever Sweets. All rights reserved.</h3>
+</footer>
--- index.php
+++ index.php
@@ -2,21 +2,21 @@
 
     $menu = [
         $icecream = [ # Ice Cream Menu
-            ['name' => 'Vanilla Ice Cream',         'price' => 50, 'quantity' => 100],
+            ['name' => 'Vanilla Ice Cream',         'price' => 50, 'quantity' => 0],
             ['name' => 'Chocolate Ice Cream',       'price' => 60, 'quantity' => 80],
             ['name' => 'Coffee Fudge Ice Cream',    'price' => 55, 'quantity' => 90],
             ['name' => 'Ube Cheese Ice Cream',      'price' => 65, 'quantity' => 70]
         ],
         $cake = [ # Cake Menu (1 Slice only)
             ['name' => 'Chocolate Cake',        'price' => 150, 'quantity' => 40],
-            ['name' => 'Vanilla Cake',          'price' => 140, 'quantity' => 50],
+            ['name' => 'Vanilla Cake',          'price' => 140, 'quantity' => 0],
             ['name' => 'Red Velvet Cake',       'price' => 160, 'quantity' => 30],
             ['name' => 'Cheesecake ',           'price' => 250, 'quantity' => 60]
         ],
         $chocolate = [ # Chocolate Menu (1 Bar only)
             ['name' => 'Milk Chocolate',            'price' => 130, 'quantity' => 200],
             ['name' => 'Dark Chocolate',            'price' => 135, 'quantity' => 150],
-            ['name' => 'White Chocolate',           'price' => 120, 'quantity' => 180],
+            ['name' => 'White Chocolate',           'price' => 120, 'quantity' => 0],
             ['name' => 'Assorted Chocolates Box',   'price' => 400, 'quantity' => 30]
         ],
         $others = [ # Various Sweets and Pies Menu
@@ -57,102 +57,65 @@
             <tr class="table-pink">
                 <th>Dessert Item</th>
                 <th>Price (PHP)</th>
+                <th ckass="table-header-extra-space">Stock</th>
+                <th class="table-header-extra-space">Availability</th>
             </tr>
-            <tr class="table-cyan menu-section">
-                <td colspan="2">Ice Cream Menu</td>
-            </tr>
-            <tr class="table-cyan">
-                <td><?= $icecream[0]['name']; ?></td>
-                <td><?= $icecream[0]['price']; ?></td>
-            </tr>
-            <tr class="table-cyan">
-                <td><?= $icecream[1]['name']; ?></td>
-                <td><?= $icecream[1]['price']; ?></td>
-            </tr>
-            <tr class="table-cyan">
-                <td><?= $icecream[2]['name']; ?></td>
-                <td><?= $icecream[2]['price']; ?></td>
-            </tr>
-            <tr class="table-cyan">
-                <td><?= $icecream[3]['name']; ?></td>
-                <td><?= $icecream[3]['price']; ?></td>
-            </tr>
-            <tr class="table-green menu-section">
-                <td colspan="2">Cake Menu</td>
-            </tr>
-            <tr class="table-green">
-                <td><?= $cake[0]['name']; ?></td>
-                <td><?= $cake[0]['price']; ?></td>
-            </tr>
-            <tr class="table-green">
-                <td><?= $cake[1]['name']; ?></td>
-                <td><?= $cake[1]['price']; ?></td>
-            </tr>
-            <tr class="table-green">
-                <td><?= $cake[2]['name']; ?></td>
-                <td><?= $cake[2]['price']; ?></td>
-            </tr>
-            <tr class="table-green">
-                <td><?= $cake[3]['name']; ?></td>
-                <td><?= $cake[3]['price']; ?></td>
-            </tr>
-            <tr class="table-blue menu-section">
-                <td colspan="2">Chocolate Menu</td>
-            </tr>
-            <tr class="table-blue">
-                <td><?= $chocolate[0]['name']; ?></td>
-                <td><?= $chocolate[0]['price']; ?></td>
-            </tr>
-            <tr class="table-blue">
-                <td><?= $chocolate[1]['name']; ?></td>
-                <td><?= $chocolate[1]['price']; ?></td>
-            </tr>
-            <tr class="table-blue">
-                <td><?= $chocolate[2]['name']; ?></td>
-                <td><?= $chocolate[2]['price']; ?></td>
-            </tr>
-            <tr class="table-blue">
-                <td><?= $chocolate[3]['name']; ?></td>
-                <td><?= $chocolate[3]['price']; ?></td>
-            </tr>
-            <tr class="table-purple menu-section">
-                <td colspan="2">Various Sweets and Pies Menu</td>
-            </tr>
-            <tr class="table-purple">    
-                <td><?= $others[0]['name']; ?></td>
-                <td><?= $others[0]['price']; ?></td>
-            </tr>
-            <tr class="table-purple">
-                <td><?= $others[1]['name']; ?></td>
-                <td><?= $others[1]['price']; ?></td>
-            </tr>
-            <tr class="table-purple">
-                <td><?= $others[2]['name']; ?></td>
-                <td><?= $others[2]['price']; ?></td>    
-            </tr>
-            <tr class="table-purple">   
-                <td><?= $others[3]['name']; ?></td>
-                <td><?= $others[3]['price']; ?></td>
-            </tr>
+            
+
+            <?php
+                $categories = [
+                    'Ice Cream Menu' =>                 ['items' => $icecream, 'class' => 'table-cyan'],
+                    'Cake Menu' =>                      ['items' => $cake, 'class' => 'table-green'],
+                    'Chocolate Menu' =>                 ['items' => $chocolate, 'class' => 'table-blue'],
+                    'Various Sweets and Pies Menu' =>   ['items' => $others, 'class' => 'table-purple'],
+                ];
+
+                foreach ($categories as $section_name => $data) {
+                    # Section Header (Ice Cream Menu, Cake Menu, etc.)
+                    echo    "<tr class=\"{$data['class']} menu-section\">
+                            <td colspan=\"4\">{$section_name}</td></tr>";
+
+                    # Dessert Items
+                    foreach ($data['items'] as $item) {
+                        
+                        # if else to know the availability of the dessert item
+                        if ($item['quantity'] > 0) {
+                            $availability = 'Available';
+                        } else {
+                            $availability = 'Out of Stock'; 
+                        }
+
+                        echo "<tr class=\"{$data['class']}\">";
+                        echo "<td>" . $item['name'] . "</td>";
+                        echo "<td>" . $item['price'] . "</td>";
+                        echo "<td>" . $item['quantity'] . "</td>";
+                        echo "<td>" . $availability . "</td>";
+                        echo "</tr>";
+                    }
+                }
+            ?>
+            
             <tr class="table-yellow menu-section">
-                <td colspan="2">Bundle Offers (10% Discount)</td>
+                <td colspan="4">Bundle Offers (10% Discount)</td>
             </tr>
-            <tr class="table-yellow">
-                <td><?= $bundles[0]['name']; ?></td>
-                <td><?= $bundles[0]['price']; ?></td>
-            </tr>
-            <tr class="table-yellow">
-                <td><?= $bundles[1]['name']; ?></td>
-                <td><?= $bundles[1]['price']; ?></td>
-            </tr>
-            <tr class="table-yellow">
-                <td><?= $bundles[2]['name']; ?></td>
-                <td><?= $bundles[2]['price']; ?></td>
-            </tr>
-            <tr class="table-yellow">
-                <td><?= $bundles[3]['name']; ?></td>
-                <td><?= $bundles[3]['price']; ?></td>
-            </tr>
+            
+            <?php foreach ($bundles as $bundle) { # Separate foreach loop because it doesnt have a quantity and availabiliy ?>
+                <tr class="table-yellow">
+                    <td><?= $bundle['name']; ?></td>
+                    <td><?php echo $bundle['price']; ?></td>
+                    <td colspan="2">- - -</td>
+                </tr>
+            <?php } ?>
         </table>
+
+        <div id="special-offers">
+            <h2>Holiday Special Deals !</h2>
+            <h3><span>Purchasing a bundle comes<br> with a free brownie !!</span></h3>
+        </div>
+        <img id="img-bell" src="images/bell.png" alt="Dessert Image">
+        <img id="img-border" src="images/christmas_border.png" alt="Dessert Image">
+
+        <?php include 'footer.php'; ?>
+
     </body>
 </html>
