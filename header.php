70c9b9d679a9e972168ae93cbc692d1dbfa0017f
C2nstant <earljaissendizon@gmail.com>
2025-12-03 21:53:09 +0800

    Module 3 Commit
    + functions.php
    + header.php
    + stock.php
    + stock_control.php
    + Updated index.php
    + Added more css for better design
    + Removed all images in HTML

---
+++ header.php
@@ -0,0 +1,7 @@
+<header>
+    <h2>Forever Sweets</h2>
+    <nav>
+        |<a href="index.php">Menu</a>|
+        <a href="stock_control.php">Stock</a>|
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -1,31 +1,7 @@
 <?php
 
-    $menu = [
-        $icecream = [ # Ice Cream Menu
-            ['name' => 'Vanilla Ice Cream',         'price' => 50, 'quantity' => 0],
-            ['name' => 'Chocolate Ice Cream',       'price' => 60, 'quantity' => 80],
-            ['name' => 'Coffee Fudge Ice Cream',    'price' => 55, 'quantity' => 90],
-            ['name' => 'Ube Cheese Ice Cream',      'price' => 65, 'quantity' => 70]
-        ],
-        $cake = [ # Cake Menu (1 Slice only)
-            ['name' => 'Chocolate Cake',        'price' => 150, 'quantity' => 40],
-            ['name' => 'Vanilla Cake',          'price' => 140, 'quantity' => 0],
-            ['name' => 'Red Velvet Cake',       'price' => 160, 'quantity' => 30],
-            ['name' => 'Cheesecake ',           'price' => 250, 'quantity' => 60]
-        ],
-        $chocolate = [ # Chocolate Menu (1 Bar only)
-            ['name' => 'Milk Chocolate',            'price' => 130, 'quantity' => 200],
-            ['name' => 'Dark Chocolate',            'price' => 135, 'quantity' => 150],
-            ['name' => 'White Chocolate',           'price' => 120, 'quantity' => 0],
-            ['name' => 'Assorted Chocolates Box',   'price' => 400, 'quantity' => 30]
-        ],
-        $others = [ # Various Sweets and Pies Menu
-            ['name' => 'Brownie',               'price' => 50, 'quantity' => 100],
-            ['name' => 'Glazed Donut',          'price' => 25, 'quantity' => 200],
-            ['name' => 'Chocolate Chip Cookie', 'price' => 15, 'quantity' => 150],
-            ['name' => 'Apple Pie',             'price' => 385, 'quantity' => 75]
-        ]
-    ];
+include_once ("stock.php");
+
 
     # Bundle Offers Computation
     $discount_rate = 0.10; # 10% Discount
@@ -35,7 +11,8 @@
     $chocolate_total = array_sum(array_column($chocolate, 'price'));
     $others_total = array_sum(array_column($others, 'price'));
 
-    $bundles = [ # Bundle Offers with Discount
+	# Bundle Offers with Discount	
+    $bundles = [ 
         ['name' => 'All Ice Cream',         'price' => ($icecream_total * (1 - $discount_rate))],
         ['name' => 'All Cakes',             'price' => ($cake_total * (1 - $discount_rate))],
         ['name' => 'All Chocolates',        'price' => ($chocolate_total * (1 - $discount_rate))],
@@ -50,15 +27,18 @@
         <link rel="stylesheet" href="css/styles.css">
     </head>
     <body>
-        <h1>Forever Sweets</h1>
+        
+        <?php include 'header.php'; ?>
+
+        <h1>Desserts Menu</h1>
 
         <!-- Dessert Menu Table -->
         <table>
             <tr class="table-pink">
-                <th>Dessert Item</th>
-                <th>Price (PHP)</th>
-                <th ckass="table-header-extra-space">Stock</th>
-                <th class="table-header-extra-space">Availability</th>
+                <th class="width-55">Dessert Item</th>
+                <th class="width-15">Price (PHP)</th>
+                <th class="width-15">Stock</th>
+                <th class="width-15">Availability</th>
             </tr>
             
 
@@ -73,7 +53,7 @@
                 foreach ($categories as $section_name => $data) {
                     # Section Header (Ice Cream Menu, Cake Menu, etc.)
                     echo    "<tr class=\"{$data['class']} menu-section\">
-                            <td colspan=\"4\">{$section_name}</td></tr>";
+                            <td colspan=\"4\"><strong>$section_name</strong></td></tr>";
 
                     # Dessert Items
                     foreach ($data['items'] as $item) {
@@ -87,7 +67,7 @@
 
                         echo "<tr class=\"{$data['class']}\">";
                         echo "<td>" . $item['name'] . "</td>";
-                        echo "<td>" . $item['price'] . "</td>";
+                        echo "<td>" . number_format($item['price'], 1) . "</td>";
                         echo "<td>" . $item['quantity'] . "</td>";
                         echo "<td>" . $availability . "</td>";
                         echo "</tr>";
@@ -109,11 +89,10 @@
         </table>
 
         <div id="special-offers">
-            <h2>Holiday Special Deals !</h2>
-            <h3><span>Purchasing a bundle comes<br> with a free brownie !!</span></h3>
+            <h2>Holiday Special Deals !!!</h2>
+            <h3><span>Starting December 1 - December 25</span></h3>
+            <h3><span>Every PHP 300 worth of items comes with a free brownie!</span></h3>
         </div>
-        <img id="img-bell" src="images/bell.png" alt="Dessert Image">
-        <img id="img-border" src="images/christmas_border.png" alt="Dessert Image">
 
         <?php include 'footer.php'; ?>
 
